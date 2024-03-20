22ed5e0872ef2c342b6b504421d30b0d126ab494
Japsert <40762304+Japsert@users.noreply.github.com>
2024-03-21 00:09:23 +0100

    Duct tape fix 👍

--- 1/redirections.h
+++ 1/redirections.h
@@ -1,6 +1,8 @@
 #ifndef REDIRECTIONS_H
 #define REDIRECTIONS_H
 
+#include <stdbool.h>
+
 /**
  * <redirections> ::= "<" <filename> ">" <filename>
  *                 |  ">" <filename> "<" <filename>
@@ -16,6 +18,6 @@ typedef struct Redirections {
 Redirections *redirections_create();
 void redirections_destroy(Redirections **redirections);
 void redirections_print(Redirections *redirections, int depth);
-void redirections_execute(Redirections *redirections, int *infd, int *outfd);
+bool redirections_execute(Redirections *redirections, int *infd, int *outfd);
 
 #endif // REDIRECTIONS_H
